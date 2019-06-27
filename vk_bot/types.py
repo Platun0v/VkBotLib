1beab448a21e5a2d530b0ebc5c7aa34c604dffb2
Platun0v <nick285961@gmail.com>
2019-06-27 19:36:55 +0300

    Done Message type
    Complete making Message from json

--- vk_bot/types.py
+++ vk_bot/types.py
@@ -6,11 +6,28 @@ class Message:
     def from_json(cls, json_string):
         obj = json.loads(json_string)
 
-        return cls(obj)
+        message_id = obj['id']
+        date = obj['date']
+        peer_id = obj['peer_id']
+        from_id = obj['from_id']
+        text = obj['text']
+        ref = ['ref']
+        ref_source = obj['ref_source']
+        attachments = obj['attachments']
+        geo = obj['geo']
+        payload = obj['payload']
+        fwd_messages = ['fwd_messages']
+        reply_messages = obj['reply_messages']
+        action = obj['action']
+
+        return cls(message_id, date, peer_id, from_id, text, ref, ref_source, attachments, geo, payload,
+                   fwd_messages, reply_messages, action, obj)
 
     def __init__(self, message_id, date, peer_id, from_id, text, ref, ref_source, attachments, geo, payload,
-                 fwd_messages, reply_messages, action):
-        self.message_id = message_id
+                 fwd_messages, reply_messages, action, raw=None):
+        self.raw = raw
+
+        self.id = message_id
         self.date = date
         self.peer_id = peer_id
         self.from_id = from_id
